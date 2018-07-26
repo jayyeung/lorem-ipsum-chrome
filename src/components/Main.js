c45fd6f45039f76ba3a46a5d5dad9611771b4dc7
Jason Yeung <jsy9@sfu.ca>
2018-07-26 11:07:31 -0700

    implement copy & select

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,9 +6,35 @@ import { LoremInput } from './LoremInput';
 @inject('MainStore')
 @observer
 export class Main extends Component {
+	copyArea = React.createRef()
 
-	onParams = (val, type) => {
-		console.log(type, val);
+	state = {
+		customSelect: false
+	}
+
+	copySelected = () => {
+		if (!this.state.customSelect)
+			console.log(this.copyArea.current.select());
+		document.execCommand('copy');
+	}
+
+	selectCopy = ({ target }) => {
+		const start = target.selectionStart;
+		const end = target.selectionEnd;
+
+		console.log('start', start, 'end', end);
+
+		if ((start !== 0 && end < this.props.MainStore.lorem.length) && start !== end)
+			this.setState({ customSelect: true });
+		else
+			this.setState({ customSelect: false });
+	}
+
+	onFocus = (e) => {
+		e.target.select();
+	}
+
+	setParams = (val, type) => {
 		this.props.MainStore.params[type] = val;
 		this.props.MainStore.getLorem();
 	}
@@ -16,9 +42,14 @@ export class Main extends Component {
 	render() {
 		return (
 			<div className='c-main'>
-				<LoremInput onParams={this.onParams}/>
+				<LoremInput onParams={this.setParams}/>
+				<button onClick={this.copySelected}>
+					{(this.state.customSelect) ? 'Copy Selected' : 'Copy All'}
+				</button>
 
-				<textarea readOnly
+				<textarea ref={this.copyArea}
+				readOnly onFocus={this.onFocus}
+				onSelect={this.selectCopy}
 				value={this.props.MainStore.lorem}>
 				</textarea>
 
