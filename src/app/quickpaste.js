796d414dbaabecab4865c0335d7cccf44ba0780c
Jason Yeung <jsy9@sfu.ca>
2018-07-27 10:25:53 -0700

    add context menu

---
+++ src/app/quickpaste.js
@@ -0,0 +1,51 @@
+const common = {
+	contexts: ['editable'],
+	onclick: quickPaste
+}
+
+function quickPaste() {
+	alert('yes');
+}
+
+// Context Menus
+const menuConfig = {
+	title: 'Lorem Clipsum - Quick Paste',
+	id: 'quick-paste',
+	...common
+};
+
+const separator = {
+	type: 'separator',
+	parentId: 'quick-paste',
+	id: 'separate'
+};
+
+const defaultPaste = {
+	title: 'Paste with current settings',
+	parentId: 'quick-paste',
+	id: 'default-paste',
+	...common
+};
+
+chrome.contextMenus.create(menuConfig);
+chrome.contextMenus.create(defaultPaste);
+chrome.contextMenus.create(separator);
+
+// Custom Options
+let customPaste = {
+	title: '5 paragraphs, 50 words each',
+	parentId: 'quick-paste',
+	id: 'custom-paste',
+	...common
+};
+
+const storageArea = chrome.storage.sync;
+storageArea.get('quick_paste_options', (data) => {
+	data.forEach(() => {
+		chrome.contextMenus.create(customPaste);
+	});
+});
+
+
+
+
