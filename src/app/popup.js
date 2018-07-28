1a72fb971bfcb210b192d8ab88010951626d6faa
Jason Yeung <jsy9@sfu.ca>
2018-07-27 17:17:52 -0700

    add style

--- src/app/popup.js
+++ src/app/popup.js
@@ -4,7 +4,27 @@ import ReactDOM from 'react-dom';
 class Popup extends Component {
 	render() {
 		return (
-			<div>Test</div>
+			<div className='c-popup'>
+				<div className='c-popup__params o-panel'>
+					<div className='o-panel__fluid'>
+						<input type='number' value='5' min='1' max='30'/>
+					</div>
+					<button>Copy Text</button>
+				</div>
+
+				<div className='o-panel'>
+					<div className='o-panel__fluid'>
+						<strong>5</strong> Paragraphs, <strong>50</strong> words each
+					</div>
+					<button className='o-link'>Settings</button>
+				</div>
+
+				<div className='c-popup__textarea'>
+					<textarea spellCheck='false'
+					defaultValue='Repellendus, placeat.'>
+					</textarea>
+				</div>
+			</div>
 		);
 	}
 };
