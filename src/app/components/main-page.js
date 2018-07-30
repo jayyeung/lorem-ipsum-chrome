dd735618ffd8841e808b57f8fe9367a96fa24850
Jason Yeung <jsy9@sfu.ca>
2018-07-29 17:56:46 -0700

    implement placeholder text copy

--- src/app/components/main-page.js
+++ src/app/components/main-page.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
 
 export default class MainPage extends Component {
+	state = {
+		selectedCopy: false,
+		copyOutput: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, debitis! Eligendi neque quisquam dignissimos est sit rerum soluta dolore ullam repellat vitae incidunt tenetur ex, aliquid delectus aliquam sed. Nisi, rerum deleniti. Beatae autem error atque accusantium quae, harum explicabo doloremque recusandae doloribus aliquam nisi sint quisquam maiores corporis voluptatem?'
+	};
+
+	textArea = React.createRef();
+	componentDidMount() {
+		// onSelect doesn't work with ShadowDOM due to
+		// event bubbling issues
+		(this.textArea).current.addEventListener('select', this.onSelectCopy);
+	}
+
+	selectAllCopy = () => { (this.textArea).current.select(); }
+	onUnselectCopy = () => { this.setState({selectedCopy: false}); }
+
+	onSelectCopy = ({target}) => {
+		const copyLen = (this.state.copyOutput).length;
+		const selStart = target.selectionStart;
+		const selEnd = target.selectionEnd;
+
+		if ((selStart === 0 && selEnd === copyLen) || (selStart === selEnd))
+			this.setState({selectedCopy: false});
+		else this.setState({selectedCopy: true});
+	}
+
+	copyText = () => {
+		if (!this.state.selectedCopy)
+			(this.textArea).current.select();
+		document.execCommand('copy');
+	}
+
 	render() {
 		return (
-			<div class='c-modal-main'>
+			<div className='c-modal-main'>
 				<div className='c-modal__panel u-pr-16'>
 					<div className='o-media__fluid'>
 						<input id='pgraphs' type='number' min='1' max='15'/>
-						<label for='pgraphs' class='u-mr-28'>Paragraphs</label>
+						<label htmlFor='pgraphs' className='u-mr-28'>Paragraphs</label>
 
 						<input id='words' type='number' min='1' max='500'/>
-						<label for='words'>Words each</label>
+						<label htmlFor='words'>Words each</label>
 					</div>
 
-					<button clasaName='c-modal-main__copy'>Copy Text</button>
+					<button className='c-modal-main__copy'
+					onClick={this.copyText}>
+						{(this.state.selectedCopy) ? 'Copy selected' : 'Copy all'}
+					</button>
 				</div>
 
 				<div className='c-modal__panel'>
 					<div className='o-media__fluid'>
 						<input id='include-p' type='checkbox'/>
-						<label for='include-p'>include &lt;p&gt; tags</label>
+						<label htmlFor='include-p'>include &lt;p&gt; tags</label>
 					</div>
 
 					<button className='c-modal-main__settings'>
@@ -28,8 +62,11 @@ export default class MainPage extends Component {
 				</div>
 
 				<div className='c-modal-main__textarea'>
-					<textarea spellCheck='false'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, debitis! Eligendi neque quisquam dignissimos est sit rerum soluta dolore ullam repellat vitae incidunt tenetur ex, aliquid delectus aliquam sed. Nisi, rerum deleniti. Beatae autem error atque accusantium quae, harum explicabo doloremque recusandae doloribus aliquam nisi sint quisquam maiores corporis voluptatem?
+					<textarea readOnly spellCheck='false'
+					ref={this.textArea}
+					onFocus={this.selectAllCopy}
+					onMouseUp={this.onUnselectCopy}
+					value={this.state.copyOutput}>
 					</textarea>
 				</div>
 			</div>
